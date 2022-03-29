7f989af510c0be78e33019299883b3574cfcd198
lixin yang <2690688423@qq.com>
2022-03-29 12:56:46 +0800

    update db

--- main.go
+++ main.go
@@ -4,7 +4,58 @@ import (
 	"fmt"
 	"os"
 	"encoding/json"
+	"sync"
+	"github.com/jcelliott/lumber"
 )
+
+const Version = "1.0.0"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir string
+		log Logger
+	}
+
+	Options struct {
+		Logger
+	}
+)
+
+func New(dir string, options *Options) (*Driver, error) {
+
+}
+
+func (d *Driver) Write() error {
+
+}
+
+func (d *Driver) Read() error {
+
+}
+
+func (d *Driver) ReadAll() () {
+
+}
+
+func (d *Driver) Delete() error {
+
+}
+
+func (d *Driver) getOrCreateMutex() *sync.Mutex {
+
+}
+
 
 type User struct {
 	Name string 
@@ -30,7 +81,7 @@ func main() {
 	}
 
 	empolyees := []User{
-		
+		{"John", "23", "2312312321", "Google", Address{"san francisco", "guiug", "USA", "123123"}},
 	}
 
 	for _, value := range empolyees {
@@ -48,5 +99,22 @@ func main() {
 		fmt.Println("Error", err)
 	}
 	fmt.Println(records)
+
+	allusers := []User{}
+	for _, f := range records {
+		empolyeeFound := User{}
+		if err := json.Unmarshal([]byte(f), &empolyeeFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		allusers = append(allusers, empolyeeFound)
+	}
+	fmt.Println((allusers))
+
+	if err := db.Delete("user", "john"); err != nil {
+		fmt.Println("Error", err)
+	}
+	if err := db.Delete("user", "john"); err != nil {
+
+	}
 }
 
